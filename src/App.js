c8808d61676a399a9465eef3c6d9dad869d6ef6d
Adri-ESM <anggivmorales@gmail.com>
2023-03-08 15:01:19 -0300

    modificando css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css'
 import Card from './components/Card.jsx'
 import Cards from './components/Cards.jsx'
 import SearchBar from './components/SearchBar.jsx'
-import characters, { Rick } from './data.js'
+import characters from './data.js'
 import ImageHeader from './images/Rick-and-Morty.png'
 
 function App () {
@@ -17,21 +17,11 @@ function App () {
         />
       </div>
       <div>
-        <Card
-          name={Rick.name}
-          species={Rick.species}
-          gender={Rick.gender}
-          image={Rick.image}
-          onClose={() => window.alert('Emulamos que se cierra la card')}
-        />
-      </div>
-      <hr />
-      <div>
         <Cards
           characters={characters}
         />
       </div>
-      <hr />
+      
     </div>
   )
 }
