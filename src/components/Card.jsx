5cda0f72b2d98c2ef7b66ad1f3d36b4f935a2d4c
Adri-ESM <anggivmorales@gmail.com>
2023-03-08 14:54:40 -0300

    modificando css

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,24 +9,17 @@
             </div>
             <div className="card-body-btn">
                <div className="card-body">
-                  <div className="card-title">
-                     <h3>Name: {name}</h3>
+                  <h3 className="card-title">Name: {name}</h3>
+                  <div className="card-subtitle"></div>
+                  <p className="card-species">Specie: {species}</p>
+                  <p className="card-gender">Gender: {gender}</p>
                   </div>
-                  <div className="modal is open">
-                     <div className="modal-container">
-                        <button onClick={onClose} className="modal-close">X</button>
-                        <p>Specie: {species}</p>
-                        <p>Gender: {gender}</p>
-                     </div>
-                  </div>
-                  {/*<div>
-                      <button onClick={onClose} className="card-close-btn">
-                        <span>X</span>
-                     </button> 
-                  </div>*/}
+                  <button onClick={onClose} className="card-close-btn">
+                     <span>X</span>
+                  </button>
                </div>
             </div>
-         </div>
+
    );
 }
 
