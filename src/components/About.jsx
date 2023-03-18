dc0349991d2a1cb482943b47ce9400a25854672b
Adri-ESM <anggivmorales@gmail.com>
2023-03-17 23:54:57 -0300

    Agregando info al about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,16 @@ const About = () => {
     return <div className='container'>
         <h1>Hola Soy Adriana</h1>
         <h2>Full Stack Developer</h2>
-        <p>Me encanta Programar, tanto como el Crossfit
+        <p className="container_p1">Me encanta Programar, tanto como el Crossfit
             entre a Henry por la oportunidad que vi, de un 
             brillante futuro en Programación.
+            Soy profesional en el area de Recursos Humanos,
+            y es por esto que me encanta trabajar en equipo,
+            y juntos alcanzar las metas propuestas.
+        </p>
+        <p className="container_p2">
+            Aprender y enseñar con código se ha convertido 
+            en mi pasión.
         </p>
     </div>
 }
