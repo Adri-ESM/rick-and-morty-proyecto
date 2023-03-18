ad76c67ca4929bb88f35bd65216db2689f9eea43
Adri-ESM <anggivmorales@gmail.com>
2023-03-17 23:27:00 -0300

    Agregando info al about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,12 @@ import React from "react";
 
 const About = () => {
     return <div className='container'>
-        <h1>Este es el Componente About</h1>
+        <h1>Hola Soy Adriana</h1>
+        <h2>Full Stack Developer</h2>
+        <p>Me encanta Programar, tanto como el Crossfit
+            entre a Henry por la oportunidad que vi, de un 
+            brillante futuro en Programación.
+        </p>
     </div>
 }
 
